431f359f4b09538777784f8b271e459e6d22b1c3
Kevin Pfefferle <kevin@pfefferle.co>
2016-10-03 19:57:35 -0400

    Update component test

--- tests/integration/pods/components/buy-now/component-test.js
+++ tests/integration/pods/components/buy-now/component-test.js
@@ -11,14 +11,5 @@ test('it renders', function(assert) {
 
   this.render(hbs`{{buy-now}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#buy-now}}
-      template block text
-    {{/buy-now}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), 'Buy Now!');
 });
