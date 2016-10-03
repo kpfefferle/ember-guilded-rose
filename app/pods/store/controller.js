825c1765b405495168046640f371b1dc4264e6f3
Kevin Pfefferle <kevin@pfefferle.co>
2016-10-03 19:39:02 -0400

    Return data in route

--- app/pods/store/controller.js
+++ app/pods/store/controller.js
@@ -1,38 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  itemTypes: [
-    {
-      name: "+5 Dexterity Vest",
-      cost: 15,
-      options: { size: 'M', color: 'Blue' }
-    },
-    {
-      name: "Aged Brie",
-      cost: 20,
-      options: { aged: false, age: null }
-    },
-    {
-      name: "Elixir of the Mongoose", 
-      cost: 10,
-      options: { strength: 1}
-    },
-    {
-      name: "Sulfuras, Hand of Ragnaros", 
-      cost: 10,
-      options: { size: 'M' }
-    },
-    {
-      name: "Backstage passes to a TAFKAL80ETC concert", 
-      cost: 10,
-      options: {}
-    },
-    {
-      name: "Conjured Mana Cake",
-      cost: 10,
-      options: { flavor: 'chocolate' }
-    }
-  ],
+  items: Ember.computed.alias('model'),
 
   cartItems: [
   ],
--- app/pods/store/route.js
+++ app/pods/store/route.js
@@ -1,4 +1,38 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return [
+      {
+        name: "+5 Dexterity Vest",
+        cost: 15,
+        options: { size: 'M', color: 'Blue' }
+      },
+      {
+        name: "Aged Brie",
+        cost: 20,
+        options: { aged: false, age: null }
+      },
+      {
+        name: "Elixir of the Mongoose",
+        cost: 10,
+        options: { strength: 1}
+      },
+      {
+        name: "Sulfuras, Hand of Ragnaros",
+        cost: 10,
+        options: { size: 'M' }
+      },
+      {
+        name: "Backstage passes to a TAFKAL80ETC concert",
+        cost: 10,
+        options: {}
+      },
+      {
+        name: "Conjured Mana Cake",
+        cost: 10,
+        options: { flavor: 'chocolate' }
+      }
+    ];
+  }
 });
