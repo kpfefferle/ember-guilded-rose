68adee60291cf530547d519e9a2afb24866e34a5
Kevin Pfefferle <kevin@pfefferle.co>
2016-10-03 20:13:19 -0400

    Compute presence of size option

--- app/pods/components/shelf-item/component.js
+++ app/pods/components/shelf-item/component.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['shelf-item']
+  classNames: ['shelf-item'],
+
+  hasSizeOption: Ember.computed('item.options', function() {
+    let item = this.get('item');
+    return item.options.size !== undefined;
+  })
 });
