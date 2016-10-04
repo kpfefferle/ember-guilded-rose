5151e995ac28ab8611d984ddd5602ba28e17b759
Kevin Pfefferle <kevin@pfefferle.co>
2016-10-03 20:44:55 -0400

    Clean up component

--- app/pods/components/buy-now/component.js
+++ app/pods/components/buy-now/component.js
@@ -1,26 +1,28 @@
 import Ember from 'ember';
 
-let passName = 'Backstage passes to a TAFKAL80ETC concert';
+const passName = 'Backstage passes to a TAFKAL80ETC concert';
 
 export default Ember.Component.extend({
     tagName: 'button',
-    buyMessage: 'Buy Now!',
     attributeBindings: ['cantBuy:disabled'],
+
+    buyMessage: 'Buy Now!',
+
     cantBuy: Ember.computed.not('canBuy'),
-    cantBuyMessage: Ember.computed('item.name', function () {
-        if (this.get('item.name') === passName) {
-            return 'Buy the Hand of Ragnaros first';
-        } else {
-            return 'Not for sale';
-        }
-    }),
 
     canBuy: Ember.computed('item.name', 'cartItems.@each.name', function () {
-        if (this.get('item.name') === passName) {
-            return this.get('cartItems').isAny('name', 'Sulfuras, Hand of Ragnaros');
-        } else {
-            return true;
-        }
+      if (this.get('item.name') === passName) {
+          return this.get('cartItems').isAny('name', 'Sulfuras, Hand of Ragnaros');
+      } else {
+          return true;
+      }
+    }),
+    cantBuyMessage: Ember.computed('item.name', function () {
+      if (this.get('item.name') === passName) {
+          return 'Buy the Hand of Ragnaros first';
+      } else {
+          return 'Not for sale';
+      }
     }),
 
     click() {
